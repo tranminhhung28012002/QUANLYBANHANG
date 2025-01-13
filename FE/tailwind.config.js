6284a9faea65da81d78ee2b7f0e1ba8a03862507
tranminhhung28012002 <himden9999@gmail.com>
2025-01-13 19:33:38 +0700

    first

---
+++ FE/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        custom: "0px 0px 9px 2px rgba(0, 0, 0, 0.31)",
+      },
+    },
+  },
+  plugins: [],
+};
